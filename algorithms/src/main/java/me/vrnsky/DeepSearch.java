6a9b883d489f3f3b221d8b9861d5dea0a0e40128
Yegor Voronyansky <vrnsky@protonmail.ch>
2018-12-17 23:30:23 +0300

    Add deep search algo

---
+++ algorithms/src/main/java/me/vrnsky/DeepSearch.java
@@ -0,0 +1,17 @@
+package me.vrnsky;
+
+import java.util.List;
+import java.util.Map;
+
+public class DeepSearch {
+
+    public String search(Map<String, List<String>> map, String searchValue, String root) {
+        List<String> names = map.get(root);
+        for (String name : names) {
+            if (name.equalsIgnoreCase(searchValue)) {
+                return root;
+            }
+        }
+        return search(map, searchValue, names.iterator().next());
+    }
+}
---
+++ algorithms/src/test/java/me/vrnsky/DeepSearchTest.java
@@ -0,0 +1,30 @@
+package me.vrnsky;
+
+import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+public class DeepSearchTest {
+
+    @Test
+    public void whenSearchInDeepShouldCheckThatSearchFindValue() {
+        Map<String, List<String>> graph = new HashMap<>();
+        graph.put("Egor Voronyansky", Arrays.asList("Anton Treskin", "Zheka Dmitriev", "Mukha"));
+
+        // first layer connection
+        graph.put("Anton Treskin", Arrays.asList("Alexandra Maltseva", "Roman Chirko", "Dima Zimonin"));
+        graph.put("Zheka Dmitriev", Arrays.asList("Nastya Anysurname", "Masha Pasha"));
+
+        // second layer connection
+        graph.put("Alexandra Maltseva", Arrays.asList("Vasilisa Grachevskaya"));
+
+        DeepSearch deepSearch = new DeepSearch();
+        assertThat(deepSearch.search(graph, "Vasilisa Grachevskaya", "Egor Voronyansky"), is("Alexandra Maltseva"));
+    }
+}
